780c74d6c48fae62aeb794b7c9031ee5e6d31b84
Gabriel/Korver <gabrielkorver76ers@gmail.com>
2024-12-17 13:54:26 -0300

    Alteracao na logica de data

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 function generateOrderNumber() {
     const orderNumber = Math.floor(Math.random() * 1000000);
     document.getElementById('orderNumber').textContent = orderNumber;
+
+    let resultado = document.querySelector("#res");
+
+    let dataAtual = new Date();
+    let dia = dataAtual.getDate().toString().padStart(2, '0'); // Adiciona zero à esquerda se necessário
+    let mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0'); // Adiciona zero à esquerda se necessário
+    let ano = dataAtual.getFullYear();
+
+    resultado.innerHTML = `${dia}/${mes}/${ano}`
+
 }
 
 // Função para imprimir
@@ -9,5 +19,3 @@ function printPage() {
     window.print();
 }
 
-// Gera o número assim que a página carrega
-window.onload = generateOrderNumber;
